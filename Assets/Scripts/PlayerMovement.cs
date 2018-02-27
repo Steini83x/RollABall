a898c57cae111a6c032a60f491cb19ea136065d9
Steini83x <patrick.steinmueller@web.de>
2018-02-27 20:30:41 +0100

    Whole Project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rbPlayer;
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rbPlayer.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rbPlayer.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rbPlayer.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rbPlayer.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
