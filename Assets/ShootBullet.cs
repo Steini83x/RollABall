74c9ed8883f104cd2c9e16ac02d44d32aafa562a
Steini83x <patrick.steinmueller@web.de>
2018-03-01 21:02:24 +0100

    Schießen hinzugefügt

---
+++ Assets/ShootBullet.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+
+public class ShootBullet : MonoBehaviour
+{
+
+    public GameObject bulletPrefab;
+    public float bulletForwardForce = 100f;
+    public Transform playerTransform;
+    public Vector3 offsetBullet;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // if (Input.GetKeyDown("space"))
+        if (Input.GetKey(KeyCode.Space))
+        {
+            var bullet = Instantiate(bulletPrefab, playerTransform.position + offsetBullet, playerTransform.rotation);
+            bullet.GetComponent<Rigidbody>().AddForce(0, 0, bulletForwardForce * Time.deltaTime);
+        }
+    }
+
+
+}
